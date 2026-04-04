5319bf5f11fa479b4d8fb375ef7633220a017230
Suhani Hunusnale <suhanihunusnale@gmail.com>
2026-04-04 16:36:11 +0530

    Update covid_dashboard.py

--- covid_dashboard.py
+++ covid_dashboard.py
@@ -1,34 +1,32 @@
 import streamlit as st
 import pandas as pd
 
-# Load dataset
+
 data = pd.read_csv("coronavirus_daily_data.csv")
 
-# Convert date column to datetime
 data["date"] = pd.to_datetime(data["date"])
 
-# Title
+
 st.title("COVID-19 Global Dashboard")
 
-# Country selector
 country = st.selectbox("Select Country", data["country"].unique())
 
-# Filter dataset for selected country
+
 filtered = data[data["country"] == country]
 
-# Graph 1: Total Cases Over Time
+# 1.Total Cases Over Time
 st.subheader("Total Cases Over Time")
 st.line_chart(filtered.set_index("date")["cumulative_total_cases"])
 
-# Graph 2: Daily New Cases
+# 2.Daily New Cases
 st.subheader("Daily New Cases")
 st.line_chart(filtered.set_index("date")["daily_new_cases"])
 
-# Graph 3: Daily New Deaths
+# 3. Daily New Deaths
 st.subheader("Daily New Deaths")
 st.line_chart(filtered.set_index("date")["daily_new_deaths"])
 
-# Graph 4: Top 10 Countries by Total Cases
+#4. Top 10 Countries by Total Cases
 st.subheader("Top 10 Countries by Total Cases")
 
 top_countries = (
@@ -40,7 +38,7 @@ top_countries = (
 
 st.bar_chart(top_countries)
 
-# Graph 5: Country Comparison
+# 5.Country Comparison
 st.subheader("Compare Countries")
 
 countries = st.multiselect(
